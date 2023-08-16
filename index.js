aa8605c421e7048cc693e4e070e67a5b18529bb6
William Gayk <gayk.william@gmail.com>
2023-08-16 08:10:13 -0700

    ImplementedReadmeOutputAndTested

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
-// Required packages
+// Required packages and files
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
 const inquirer = require('inquirer');
 const fs = require('fs');
 
 
+// Individual projectQuestion object
 let projectQuestion = {
     question: '',
     varName: '',
     type: '',
     choices: []
 };
-
+// Array of projectQuestion objects
 let projectQuestions = [];
 
 
+// Inquirer prompt object
 let prompt = {
     type: '',
     message: '',
     name: '',
     choices: []
 }
-
+// Inquire prompt objectS
 let prompts = [];
 
 
+// license Options used in inquirer prompt
+// The values in this array must match the values in the licenseInfo array's first item and in the same order
 const licenseOptions = [`MIT`, `Apache`, `GPL`, `BSD`, `None`];
-
 let license = {
     licenseName: '',
     licenseBadge: '',
@@ -36,38 +39,31 @@ let license = {
 let licenses = [];
 
 
-
-const xlicenseBadges = [`[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`,
-                       `[![License: Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`,   
-                       `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`,
-                       `[![License: BSD](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`,
-                       ``];
-                        
 function loadLicenseInfo() {
     // Dev Note: This table must be in the same order as the licenseOptions array and 
     //             contain the same licenses
     
-    license.licenseName = 'MIT';
+    license.licenseName = licenseOptions[0]; // MIT
     license.licenseBadge = 'https://img.shields.io/badge/License-MIT-yellow.svg';
     license.licenseLink = 'https://opensource.org/licenses/MIT';
     licenses.push(JSON.parse(JSON.stringify(license)));
 
-    license.licenseName = 'Apache';
+    license.licenseName =  licenseOptions[1]; // Apache
     license.licenseBadge = 'https://img.shields.io/badge/License-Apache%202.0-blue.svg';
     license.licenseLink = 'https://opensource.org/licenses/Apache-2.0';
     licenses.push(JSON.parse(JSON.stringify(license)));
 
-    license.licenseName = 'GPL';
+    license.licenseName = licenseOptions[2]; // GPL
     license.licenseBadge = 'https://img.shields.io/badge/License-GPLv3-blue.svg';
     license.licenseLink = 'https://www.gnu.org/licenses/gpl-3.0';
     licenses.push(JSON.parse(JSON.stringify(license)));
 
-    license.licenseName = 'BSD';
+    license.licenseName = licenseOptions[3]; // BSD
     license.licenseBadge = 'https://img.shields.io/badge/License-BSD%203--Clause-blue.svg';
     license.licenseLink = 'https://opensource.org/licenses/BSD-3-Clause';
     licenses.push(JSON.parse(JSON.stringify(license)));
 
-    license.licenseName = 'None';
+    license.licenseName = licenseOptions[4]; // None
     license.licenseBadge = '';
     license.licenseLink = '';
     licenses.push(JSON.parse(JSON.stringify(license)));
@@ -166,25 +162,39 @@ function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) => {console.log(err)});
 }
 
+
 // Initialize Routine will call the functions to load the questions, ask the questions, paginate the results, and write the file
 const init = async () => {
     // Populate Questions
     loadQuestions();
     
-    // Populate License Info
+    // Populate License Info: Name, Badge, Link
     loadLicenseInfo();
 
     // Ask Questions
     const response = await inquirer.prompt(prompts);
 
-    // Generate Markdown
-    const data = generateMarkdown(response);
+    // Search for license in licenseOptions array
+    license.licenseName = '';
+    license.licenseBadge = '';
+    license.licenseLink = '';
+    for(let i=0; i < licenses.length; i++) {
+        if (licenses[i].licenseName === response.projectLicense) {
+            license = licenses[i];
+            break;
+        }
+    }
+            
+    // Generate Markdown from response and license objects
+    const data = generateMarkdown(response, license);
 
 
-    // write markdown to file
-    writeToFile('README_test.md', data);
+    // write README.MD markdown to file to
+    // application install directory 
+    writeToFile('README.MD', data);
 
 }
+
 
 // Function call to initialize app
 init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,70 @@
+function renderLicenseBadge(licenseBadge) {
+  if(licenseBadge === '' || licenseBadge === null)
+  {
+    return ``;
+  }
+
+  return `![License](${licenseBadge})`;
+
+}
+
+function renderLicenseLink(licenseLink) {
+  if (licenseLink === '' || licenseLink === null) 
+  {
+    return ``;
+  } 
+
+  return `(${licenseLink})`;
+}
+
+function renderLicenseSection(licenseName) {
+  if (licenseName === '' || licenseName === null) 
+  {
+    return ``;
+  }
+
+  return `[${licenseName}]`;
+}
+
+function generateMarkdown(data, license) {
+  return `# ${data.projectTitle} \n
+  ## Description \n
+  ${renderLicenseBadge(license.licenseBadge)} \t
+
+  ${data.projectDescription} \n
+
+  ## Table of Contents \n
+  * [Installation](#installation) \n
+  * [Usage](#usage) \n
+  * [License](#license) \n
+  * [Contributing](#contributing) \n
+  * [Tests](#tests) \n
+  * [Questions](#questions) \n
+
+  ## Installation \n
+  ${data.projectInstallation} \n
+
+  ## Usage \n
+  ${data.projectUsage} \n
+
+  ## License \n 
+  ${renderLicenseSection(license.licenseName)}${renderLicenseLink(license.licenseLink)} \n
+
+
+  ## Contributing \n
+  ${data.projectContribution} \n
+
+  ## Tests \n
+  ${data.projectTest} \n  
+
+  ## Questions \n
+  ${data.projectGithub} \n
+
+  ${data.projectEmail} \n
+
+
+`
+;
+}
+
+module.exports = generateMarkdown;
